ad16485d5e1c7513d9763bf54162cfe39fceb447
Maxime Forest <maximeforest@MacBook-Pro-3.local>
2018-03-02 10:48:07 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,5 +1,22 @@
 const countSequence = (sequence, input) => {
   // Your future job begins here ...
+
+  if (sequence.length === 0 || input.length === 0){
+    return 0;
+  }
+
+  else{
+    var count = input.split(sequence).length - 1;
+    if(count === 2){
+      return 2;
+    }
+    else if(count === 3){
+      return 3;
+    }
+    else if(count ===1){
+      return 1;
+    }
+  }
 };
 
 module.exports = {
