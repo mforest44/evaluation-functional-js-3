d0c762fbd39662ba2f7b3780e4c44b4a898b1d45
Maxime Forest <maximeforest@MacBook-Pro-3.local>
2018-03-02 10:48:48 +0100

    Exercise 2

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -5,7 +5,36 @@ describe(`${program.title}`, function () {
 
   it('Function yolo with mysterious parameter should return a list of messages', function () {
 
-    const whereIsMyMind = "???";
+    const whereIsMyMind = [
+      {
+        'sex': 1,
+        'name' : "William"
+      },
+      {
+        'sex': 1,
+        'name' : "Gaius"
+      },
+      {
+        'sex': 1,
+        'name' : "Lee"
+      },
+      {
+        'sex': 2,
+        'name' : "Kara"
+      },
+      {
+        'sex': 2,
+        'name' : "Laura"
+      },
+      {
+        'sex': 9,
+        'name' : "Number six"
+      },
+      {
+        'sex': 9,
+        'name' : "Number height"
+      }
+    ];
 
     const output = [
       "0 not known character(s) ",
