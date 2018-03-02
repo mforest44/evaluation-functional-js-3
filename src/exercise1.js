41057d50a81f1c9be92ca3375ee1f2ca7e2eae5c
Maxime Forest <maximeforest@MacBook-Pro-3.local>
2018-03-02 10:48:32 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -2,6 +2,12 @@ const moment = require("moment");
 
 const sortProduct = input => {
   // Your future job begins here ...
+  console.log('Test 1');
+  var date = input[0].dateAdded;
+  var dateM = moment(date).year();
+
+  console.log('Test ' + dateM);
+  return input.filter( value => moment(value.dateAdded).year() < 2000);
 };
 
 module.exports = {
